a667eb81c849fbce8696af997dca99ebeec9e560
riddle <krzysztof.lochwinowicz@gmail.com>
2013-09-13 00:34:33 +0200

    Empty QTest template added

---
+++ Test/refactorTester.cpp
@@ -0,0 +1,16 @@
+#include "refactorTester.hpp"
+
+QTEST_MAIN(RefactorTester)
+
+void RefactorTester::simpleTest()
+{
+    QVERIFY(0== 0);
+}
+
+void RefactorTester::simpleTest2()
+{
+    QVERIFY(0== 0);
+}
+
+
+#include "refactorTester.moc"
